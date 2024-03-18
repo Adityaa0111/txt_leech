1d3aeacb74fccd6b22f3e2dda6a989530b280a69
Adityaa0111 <163882216+Adityaa0111@users.noreply.github.com>
2024-03-18 22:54:11 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "24814358"
 api_hash = "28e85d071ae4d08a35ddfce90d06e667"
-bot_token = "7177999142:AAFJZWOJHosFEQOJVk3eWTeE9AnvPWOts8w"
+bot_token = "7051679591:AAE6jFXBekFuwZwg4jqOsSS2vhxf9mB92-E"
